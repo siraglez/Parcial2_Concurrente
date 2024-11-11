0fd60c4537752000e03ec6e6302196a40a8f7373
Sira <sgonzmad@myuax.com>
2024-11-11 11:48:38 +0100

    commit

---
+++ src/main/java/org/example/parcial2_concurrente/Parcial2ConcurrenteApplication.java
@@ -0,0 +1,13 @@
+package org.example.parcial2_concurrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2ConcurrenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parcial2ConcurrenteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parcial2_concurrente/Parcial2ConcurrenteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parcial2_concurrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ConcurrenteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
